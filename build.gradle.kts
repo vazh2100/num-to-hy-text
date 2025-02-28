434c4de37b439ff110334a7c85daa53c7c4fe027
vazh2100 <vazh2100@list.ru>
2025-02-28 18:51:38 +0300

    v1.0.1 Fix Java Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 }
 
 group = "com.vazh2100"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 }
 
 publishing {
